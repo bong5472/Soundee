b7a558586d2f86e35c2c6dff4a7096c31d0ec320
bong5472 <74997216+bong5472@users.noreply.github.com>
2022-10-29 20:57:46 +0900

    Add files via upload

--- colorcheck.py
+++ colorcheck.py
@@ -8,15 +8,15 @@ def color_check(target_color):
     t_mood = ''
     color_dist = 700
     for i in range(len(mood)):
-        print('check==',i)
         dist = ((t_red - mood.iloc[i][0])**2 + (t_green - mood.iloc[i][1])**2 + (t_blue - mood.iloc[i][2])**2)**(1/2)
+        # print('check==',i,dist)
+
         if color_dist > dist:
-           print('change : ',i)
+        #    print('change : ',i)
            color_dist = dist
            t_mood = mood.iloc[i][5]
     return t_mood, color_dist 
     
-print(color_check([255,10,152]))
     
     
     
---
+++ main.py
@@ -0,0 +1,11 @@
+from colorcheck import *
+from imgcheck import *
+
+src = './cafe.jpg'
+
+rgb_arr, rgb_percent = imgchecking(src)
+print(rgb_arr)
+point = []
+for i in range(len(rgb_arr)):
+    point.append([color_check(rgb_arr[i])[0],rgb_percent[i]])
+print(point)
