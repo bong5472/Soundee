e2cddfa46e329a762aec9c35cfc0a445544d4db7
bong5472 <bong5472@naver.com>
2022-11-01 23:17:54 +0900

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -10,46 +10,45 @@ from apiclient.errors import HttpError
 from oauth2client.tools import argparser
 
 
-DEVELOPER_KEY = "DEVELOPER_KEY_HERE"
+DEVELOPER_KEY = "key_here"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
 def youtube_search(options):
-  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-    developerKey=DEVELOPER_KEY)
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
 
-  # Call the search.list method to retrieve results matching the specified
-  # query term.
-  search_response = youtube.search().list(
-    q=options.q,
-    part="id,snippet",
-    maxResults=options.max_results
-  ).execute()
+    # Call the search.list method to retrieve results matching the specified
+    # query term.
+    search_response = youtube.search().list(
+        q=options.q,
+        part="id,snippet",
+        maxResults=options.max_results
+        ).execute()
 
-  videos = []
-  channels = []
-  playlists = []
+    videos = []
+    channels = []
+    playlists = []
 
   # Add each result to the appropriate list, and then display the lists of
   # matching videos, channels, and playlists.
-  for search_result in search_response.get("items", []):
-    if search_result["id"]["kind"] == "youtube#video":
-      videos.append("%s (%s)" % (search_result["snippet"]["title"],
+    for search_result in search_response.get("items", []):
+        if search_result["id"]["kind"] == "youtube#video":
+            videos.append("%s (%s)" % (search_result["snippet"]["title"],
                                  search_result["id"]["videoId"]))
-    elif search_result["id"]["kind"] == "youtube#channel":
-      channels.append("%s (%s)" % (search_result["snippet"]["title"],
+        elif search_result["id"]["kind"] == "youtube#channel":
+            channels.append("%s (%s)" % (search_result["snippet"]["title"],
                                    search_result["id"]["channelId"]))
-    elif search_result["id"]["kind"] == "youtube#playlist":
-      playlists.append("%s (%s)" % (search_result["snippet"]["title"],
+        elif search_result["id"]["kind"] == "youtube#playlist":
+            playlists.append("%s (%s)" % (search_result["snippet"]["title"],
                                     search_result["id"]["playlistId"]))
 
-  print("Videos:\n", "\n".join(videos), "\n")
-  print("Channels:\n", "\n".join(channels), "\n")
-  print("Playlists:\n", "\n".join(playlists), "\n")
+    print("Videos:\n", "\n".join(videos), "\n")
+    print("Channels:\n", "\n".join(channels), "\n")
+    print("Playlists:\n", "\n".join(playlists), "\n")
 
 
 if __name__ == "__main__":
-    argparser.add_argument("--q", help="Search term", default="맑은, 리드미컬한, 다이나믹한, 플레이리스트")
+    argparser.add_argument("--q", help="Search term", default="모던한, 젠틀한, 모던한, 플레이리스트")
     argparser.add_argument("--max-results", help="Max results", default=3)
     args = argparser.parse_args()
 
